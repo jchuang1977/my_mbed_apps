f9d1add48a45cb8f42c72e37eab3c6b7245d10c0
Andy Huang <jchuang1977@gmail.com>
2015-01-24 13:11:08 +0800

    update

--- mbed_http_server_led_rom/mbed_http_server_led_rom.cpp
+++ mbed_http_server_led_rom/mbed_http_server_led_rom.cpp
@@ -2,9 +2,9 @@
 #include "mbed_http_server_led_rom.h"
 
 #include"EthernetInterface.h"
-//#include"SDFileSystem.h"
+#include"SDFileSystem.h"
 #include"HTTPServer.h"
-//#include"FsHandler.h"
+#include"FsHandler.h"
 #include"led_handler.h"
 #include"rom.h"
 #include "temperature.h"
@@ -13,11 +13,12 @@ DigitalOut myled(LED1);
 EthernetInterface eth;
 HTTPServer  svr;
 //SDFileSystem sd(P0_18, P0_17, P0_15, P0_19, "sd");
+SDFileSystem sd(p5, p6, p7, p8, "sd");
 int main() {
     eth.init("192.168.1.13", "255.255.255.0", "192.168.1.1"); //Use DHCP
     eth.connect();
-   // HTTPFsRequestHandler::mount("/sd/", "/");
-    //svr.addHandler<HTTPFsRequestHandler>("/");
+    HTTPFsRequestHandler::mount("/sd/", "/");
+    svr.addHandler<HTTPFsRequestHandler>("/");   // look for index.html at default
 
     svr.addHandler<HTTPLedRequestHandler>("/led");
     svr.addHandler<HTTPRomRequestHandler>("/rom");
